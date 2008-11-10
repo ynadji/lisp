2af0cf14ff68787840c6fd394e73ffc394e1f154
Yacin Nadji <ynadji@iit.edu>
2008-11-10 11:05:57 -0600

    removed poor sieves, added richs nice sieve

--- nifty-funs.clj
+++ nifty-funs.clj
@@ -1,45 +1,23 @@
-(defn sieve [#^Integer n]
-  "Returns a list of all primes from 2 to n"
-  (let [root (Math/round (Math/floor (Math/sqrt n)))]
-    (loop [#^Integer i 3
-	   a (make-array Boolean n)
-	   result (list 2)]
-      (if (>= i n)
-	(reverse result)
-	(recur (+ i 2)
-	       (if (< i root)
-		 (loop [arr a
-			#^Integer inc (+ i i)
-			#^Integer j (* i i)]
-		   (if (>= j n)
-		     arr
-		     (recur (do (aset arr j true) arr)
-			    inc
-			    (+ j inc))))
-		 a)
-	       (if (not (aget a i))
-		 (conj result i)
-		 result))))))
-
-(defn unoptimized-sieve [n]
-  "Returns a list of all primes from 2 to n"
-  (let [root (Math/round (Math/floor (Math/sqrt n)))]
-    (loop [i 3
-	   a (make-array Boolean n)
-	   result (list 2)]
-      (if (>= i n)
-	(reverse result)
-	(recur (+ i 2)
-	       (if (< i root)
-		 (loop [arr a
-			inc (+ i i)
-			j (* i i)]
-		   (if (>= j n)
-		     arr
-		     (recur (do (aset arr j true) arr)
-			    inc
-			    (+ j inc))))
-		 a)
-	       (if (not (aget a i))
-		 (conj result i)
-		 result))))))
+(defn sieve [n]
+  (let [n (int n)]
+    "Returns a list of all primes from 2 to n"
+    (let [root (int (Math/round (Math/floor (Math/sqrt n))))]
+      (loop [i (int 3)
+	     a (int-array n)
+	     result (list 2)]
+	(if (>= i n)
+	  (reverse result)
+	  (recur (+ i (int 2))
+		 (if (< i root)
+		   (loop [arr a
+			  inc (+ i i)
+			  j (* i i)]
+		     (if (>= j n)
+		       arr
+		       (recur (do (aset arr j (int 1)) arr)
+			      inc
+			      (+ j inc))))
+		   a)
+		 (if (zero? (aget a i))
+		   (conj result i)
+		   result)))))))
