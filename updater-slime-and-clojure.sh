528cb3ace6d5e9c01b418f1501895f1657a5d6cf
Yacin Nadji <ynadji@iit.edu>
2009-04-07 14:20:44 -0500

    woo, clojure update script

---
+++ updater-slime-and-clojure.sh
@@ -0,0 +1,20 @@
+HOME=/Users/ynadji/Code/Lisp
+
+cd $HOME/clojure
+git pull
+ant
+
+cd $HOME/clojure-contrib
+git pull
+ant
+
+cd $HOME/slime
+git pull
+
+cd $HOME/slime/clojure-mode
+git pull
+
+cd $HOME/slime/swank-clojure
+git pull
+
+echo "Done!"
