8194f3915e781fb7fb8d58f2087ca65e88e78d75
Yacin Nadji <ynadji@iit.edu>
2009-04-14 02:29:21 -0500

    added miller-rabin primaliy test, finished 40/41

--- yacin/euler/project-euler.clj
+++ yacin/euler/project-euler.clj
@@ -1,9 +1,83 @@
-;;;; Project Euler, Clojure version!
 (clojure.core/ns yacin.euler.project-euler
-		 (:use yacin.nifty-funs))
+		 (:use yacin.nifty-funs)
+		 (:use clojure.contrib.combinatorics))
 
-(defn test [n]
-  (digits n))
+(defn problem-33 []
+  "The fraction 49/98 is a curious fraction, as an inexperienced mathematician in attempting
+   to simplify it may incorrectly believe that 49/98 = 4/8, which is correct, is obtained by cancelling the 9s.
+   We shall consider fractions like, 30/50 = 3/5, to be trivial examples.
+   There are exactly four non-trivial examples of this type of fraction, less than one in value,
+   and containing two digits in the numerator and denominator.
+   If the product of these four fractions is given in its lowest common terms, find the value of the denominator."
+  (apply *
+	 (loop [numer 10
+		denom (inc numer)
+		lst nil]
+	   
+	   (cond (curious-fraction? numer denom)
+		 (recur numer (inc denom) (conj lst (/ numer denom))) 
+		 (>= denom 99)
+		 (recur (inc numer) (+ 2 numer)	lst)
+		 (= 4 (count lst))
+		 lst
+		 true (recur numer (inc denom) lst)))))
 
-(defn main
-  (test 473289473289))
+(defn curious-reduce [nx ny]
+  (try
+   (let [sorted-x (sort nx)
+	 sorted-y (sort ny)]
+     (cond (= (first sorted-x) (first sorted-y))
+	   (/ (second sorted-x) (second sorted-y))
+	   (= (second sorted-x) (second sorted-y))
+	   (/ (first sorted-x) (first sorted-y))))
+   (catch java.lang.ArithmeticException e -1)))
+
+(defn curious-fraction? [x y]
+  (let [nx (digits x)
+	ny (digits y)]
+    (if (or (= x y)
+	    (and (zero? (second nx))
+		 (zero? (second ny))))
+      false
+      (= (/ x y)
+	 (curious-reduce nx ny)))))
+
+(defn problem-37 [n]
+"The number 3797 has an interesting property. Being prime itself, it is possible to continuously remove
+digits from left to right, and remain prime at each stage: 3797, 797, 97, and 7. Similarly we can work
+from right to left: 3797, 379, 37, and 3. Find the sum of the only eleven primes that are both truncatable
+from left to right and right to left."
+  (let [primes (sieve n)]
+    (loop [poss-trunc-primes primes
+	   sum -17]
+      (cond (empty? poss-trunc-primes)
+	    sum
+	    (truncatable-prime? (first poss-trunc-primes) primes)
+	    (recur (rest poss-trunc-primes) (+ sum (first poss-trunc-primes)))
+	    :else
+	    (recur (rest poss-trunc-primes) sum)))))
+
+(defn truncatable-prime? [x primes]
+  (loop [xlist (digits x)
+	 ylist (digits x)]
+    (if (empty? xlist)
+      true
+      (if (and (some #(= (stigid xlist) %) primes)
+	       (some #(= (stigid ylist) %) primes))
+	(recur (reverse (rest (reverse xlist)))
+	       (rest ylist))
+	false))))
+
+(defn problem-40 []
+  "Finding the nth digit of the fractional part of the irrational number."
+  (let [dstr (apply str (range 190000))
+	pos (map #(int (Math/pow 10 %)) (range 7))]
+    (reduce * (map #(Integer/parseInt (str (.charAt dstr %))) pos))))
+
+(defn problem-41 [n]
+  "What is the largest n-digit pandigital prime that exists? n = 7"
+  (let [perms (permutations (reverse (range 1 (inc n))))]
+    (take 10 (for [potential-prime
+		  (map #(BigInteger. (apply str %)) perms)
+		  :when (prime? potential-prime 10)]
+	      potential-prime))))
--- yacin/nifty_funs.clj
+++ yacin/nifty_funs.clj
@@ -1,9 +1,10 @@
-(clojure.core/ns yacin.nifty-funs)
+(clojure.core/ns yacin.nifty-funs
+		 (:use clojure.contrib.math))
 
 (defn sieve [n]
   (let [n (int n)]
     "Returns a list of all primes from 2 to n"
-    (let [root (int (Math/round (Math/ceil (Math/sqrt n))))]
+    (let [root (int (round (ceil (sqrt n))))]
       (loop [i (int 3)
 	     a (int-array n)
 	     result (list 2)]
@@ -25,8 +26,61 @@
 		   result)))))))
 
 (defn digits [n]
+  "Returns a list of the digits of a number"
   (map (fn [x] (. Integer (parseInt x)))
        (rest (. (str n) (split "")))))
 
 (defn stigid [n]
+  "The opposite of digits :)"
   (. Integer (parseInt (reduce str "" n))))
+
+(defn- factorize-out
+  "Factorizes out all x factors from n.
+Examples:
+  (factorize-out 10 2) ==> 5, because 2^1 * 5 = 10
+  (factorize-out 90 3) ==> 10, because 3^2 * 10 = 90"
+  [n x]
+  (loop [acc n exp 0]
+    (if (= 0 (mod acc x))
+      (recur (/ acc x) (inc exp))
+      (hash-map :exponent exp :rest acc))))
+
+(defn- expt-mod
+  "Equivalent to (mod (expt n e) m), but faster.
+http://en.wikipedia.org/wiki/Modular_exponentiation#An_efficient_method:_the_right-to-left_binary_algorithm"
+  [n e m]
+  (loop [r 1, b n, e e]
+    (if (= e 0)
+      r
+      (recur (if (odd? e)
+	       (mod (* r b) m)
+	       r)
+	     (mod (expt b 2) m)
+	     (bit-shift-right e 1)))))
+
+(defn prime?
+  "Checks if n is a prime using the Miller-Rabin pseudo-primality test.  Also
+   see *pseudo* and *pseudo-accuracy*."
+  [n k-in]
+  (cond
+    (< n 2)   false
+    (= n 2)   true
+    (even? n) false
+    :else (let [m (factorize-out (dec n) 2)
+		d (:rest m)
+		s (:exponent m)]
+	    (loop [k 1]
+	      (if (> k k-in)
+		true
+		(let [a (+ 2 (rand-int (- n 4)))
+		      x (expt-mod a d n)]
+		  (if (or (= x 1) (= x (dec n)))
+		    (recur (inc k))
+		    (if (loop [r 1
+			       x (expt-mod x 2 n)]
+			  (cond
+			    (or (= x 1) (>  r (dec s)))  false
+			    (= x (dec n))                true
+			    :else (recur (inc r) (mod (* x x) n))))
+		      (recur (inc k))
+		      false))))))))
