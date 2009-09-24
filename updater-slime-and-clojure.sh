edeaecd8e41ee062087717ac2a57783dd64cf891
Yacin Nadji <yacin@gatech.edu>
2009-09-24 18:15:02 -0400

    improved slime/clojure updater. if the folders dont exist, they are cloned and built.

--- updater-slime-and-clojure.sh
+++ updater-slime-and-clojure.sh
@@ -1,22 +1,51 @@
-HOME=/Users/ynadji/Code/Lisp
+HOME=/home/yacin/Code/Lisp
 
-cd $HOME/clojure
-ant clean
-git pull
-ant
+if [ -d $HOME/clojure ]; then
+	cd $HOME/clojure
+	ant clean
+	git pull
+	ant
+else
+	cd $HOME
+	git clone git://github.com/richhickey/clojure.git
+	cd clojure
+	ant
+fi
 
-cd $HOME/clojure-contrib
-ant clean
-git pull
-ant -Dclojure.jar=../clojure/clojure.jar
+if [ -d $HOME/clojure-contrib ]; then
+	cd $HOME/clojure-contrib
+	ant clean
+	git pull
+	ant -Dclojure.jar=../clojure/clojure.jar
+else
+	cd $HOME
+	git clone git://github.com/richhickey/clojure-contrib.git
+	cd clojure-contrib
+	ant -Dclojure.jar=../clojure/clojure.jar
+fi
 
-cd $HOME/slime
-git pull
+if [ -d $HOME/slime ]; then
+	cd $HOME/slime
+	git pull
+else
+	cd $HOME
+	git clone git://github.com/nablaone/slime.git
+fi
 
-cd $HOME/slime/clojure-mode
-git pull
+if [ -d $HOME/slime/clojure-mode ]; then
+	cd $HOME/slime/clojure-mode
+	git pull
+else
+	cd $HOME/slime
+	git clone git://github.com/jochu/clojure-mode.git
+fi
 
-cd $HOME/slime/swank-clojure
-git pull
+if [ -d $HOME/slime/swank-clojure ]; then
+	cd $HOME/slime/swank-clojure
+	git pull
+else
+	cd $HOME/slime
+	git clone git://github.com/jochu/swank-clojure.git
+fi
 
 echo "Done!"
